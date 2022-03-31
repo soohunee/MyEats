b021935eebce96ee8f1d9f457e7ece4ab6cb7ef2
soohunee <soohun96@g.skku.edu>
2022-03-31 20:42:22 +0900

    Init

---
+++ src/main/java/com/myeats/myeats/MyeatsApplication.java
@@ -0,0 +1,13 @@
+package com.myeats.myeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyeatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myeats/myeats/MyeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myeats.myeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
